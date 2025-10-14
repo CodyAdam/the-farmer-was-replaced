cc9860848342912eee47e5d89b7006cea6a4a89f
CodyAdam <cody.ad4@gmail.com>
2025-10-14 02:32:56 +0200

    add leadeerboard run

--- leaderboard.py
+++ leaderboard.py
@@ -1,3 +1,4 @@
 seed = 0
-run_time = simulate("test", Unlocks, {
-	Items.Hay:1000000000,Items.Wood:1000000000,Items.Weird_Substance:1000000000}, {}, seed, 1)
+#run_time = simulate("test", Unlocks, {	Items.Hay:1000000000,Items.Wood:1000000000,Items.Weird_Substance:1000000000}, {}, seed, 1232323232)
+
+leaderboard_run(Leaderboards.Maze,"run",600)
---
+++ run.py
@@ -0,0 +1,4 @@
+import harvest
+
+while True:
+	harvest.do_treasure()
